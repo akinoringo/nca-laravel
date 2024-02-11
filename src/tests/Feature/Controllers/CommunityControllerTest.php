9e1e374580e9fa0b42257ca16c7733f04027e1e3
akinoringo <wbraver@outlook.jp>
2024-02-12 08:43:16 +0900

    テストでRefreshdatabseを使用するように修正

--- src/tests/Feature/Controllers/CommunityControllerTest.php
+++ src/tests/Feature/Controllers/CommunityControllerTest.php
@@ -8,6 +8,8 @@ use Tests\TestCase;
 
 class CommunityControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * コミュニティ作成テスト
      * 正常系
@@ -15,9 +17,6 @@ class CommunityControllerTest extends TestCase
     public function test_store_success(): void
     {
         $response = $this->postJson('/api/communities', ['name' => 'テストコミュニティ']);
-
-        dd($response);
-
         $response->assertStatus(200)
             ->assertJson(['message' => 'コミュニティを作成しました']);
     }
