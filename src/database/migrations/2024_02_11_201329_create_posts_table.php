cd67fa2838e9684acc271489221bfaad296abf7b
akinoringo <wbraver@outlook.jp>
2024-02-12 10:22:37 +0900

    postsテーブルの定義を修正

--- src/database/migrations/2024_02_11_201329_create_posts_table.php
+++ src/database/migrations/2024_02_11_201329_create_posts_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->text('content');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
