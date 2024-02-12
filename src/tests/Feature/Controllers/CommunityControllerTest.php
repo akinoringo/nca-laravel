210c454f1ecbd9eda4e0fd08f230dfd5f81b9920
akinoringo <wbraver@outlook.jp>
2024-02-12 09:02:10 +0900

    コミュニティへの参加のテスト実装

--- src/tests/Feature/Controllers/CommunityControllerTest.php
+++ src/tests/Feature/Controllers/CommunityControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Controllers;
 
+use App\Models\Community;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,8 +18,33 @@ class CommunityControllerTest extends TestCase
      */
     public function test_store_success(): void
     {
+        // act
         $response = $this->postJson('/api/communities', ['name' => 'テストコミュニティ']);
+
+        // assert
         $response->assertStatus(200)
             ->assertJson(['message' => 'コミュニティを作成しました']);
+        $this->assertDatabaseHas(Community::class, ['name' => 'テストコミュニティ']);
+    }
+
+    /**
+     * コミュニティへの参加テスト
+     * 正常系
+     */
+    public function test_join_success(): void
+    {
+        // arrange
+        $user = User::factory()->create();
+        $community = Community::factory()->create();
+
+        // act
+        $response = $this->postJson("/api/communities/{$community->id}/join", ['userId' => $user->id]);
+
+        // assert
+        $response->assertStatus(200)
+            ->assertJson(['message' => 'コミュニティに所属しました。']);
+        $this->assertDatabaseHas('community_user', [
+            'community_id' => $community->id, 'user_id' => $user->id
+        ]);
     }
 }
